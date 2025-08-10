f685a1d1e533b96385c5f141a83107e985827716
Niloy Maity <niloymaity@iisc.ac.in>
2025-08-10 15:09:59 +0530

    List updated and added

---
+++ commonAnalysisCodes/informationCodes/protocolLists/allProtocolsjojotACS_Alpha_Sham_single.m
@@ -0,0 +1,15 @@
+function [expDates,protocolNames,stimType] = allProtocolsjojotACS_Alpha_Sham_single
+
+clear index; index=1;expDates{index} = '190725'; protocolNames{index} = 'GRF_001'; stimType{index} = 4; % Pre-Sham, SFOriCon,4x4x3
+clear index; index=2;expDates{index} = '190725'; protocolNames{index} = 'GRF_002'; stimType{index} = 4; % Sham, 10 Hz, 2.5 mA, SFOriCon,4x4x3
+clear index; index=3;expDates{index} = '190725'; protocolNames{index} = 'GRF_003'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=4;expDates{index} = '190725'; protocolNames{index} = 'GRF_004'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=5;expDates{index} = '190725'; protocolNames{index} = 'GRF_005'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=6;expDates{index} = '190725'; protocolNames{index} = 'GRF_006'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+
+clear index; index=7;expDates{index} = '230725'; protocolNames{index} = 'GRF_001'; stimType{index} = 4; % Pre-Sham, SFOriCon,4x4x3
+clear index; index=8;expDates{index} = '230725'; protocolNames{index} = 'GRF_002'; stimType{index} = 4; % Sham, 10 Hz, 2.5 mA, SFOriCon,4x4x3
+clear index; index=9;expDates{index} = '230725'; protocolNames{index} = 'GRF_003'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=10;expDates{index} = '230725'; protocolNames{index} = 'GRF_004'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=11;expDates{index} = '230725'; protocolNames{index} = 'GRF_005'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
+clear index; index=12;expDates{index} = '230725'; protocolNames{index} = 'GRF_006'; stimType{index} = 4; % Post-Sham, SFOriCon,4x4x3
