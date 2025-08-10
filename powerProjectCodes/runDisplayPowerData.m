bd6eaa7cac0491ffe393d363d7f0ae0a98ef87b9
Niloy Maity <niloymaity@iisc.ac.in>
2025-08-10 15:11:21 +0530

    Minor changes

--- powerProjectCodes/runDisplayPowerData.m
+++ powerProjectCodes/runDisplayPowerData.m
@@ -1,13 +1,15 @@
-subjectName = 'dona';
-folderData = 'N:\Projects\Niloy_tES-StimulationProject'; % This is the folder where the folder 'data' with segmented data is kept. We assume that badTrials are available also
+subjectName = 'jojo';
+folderData = 'Z:\Projects\ProjectVajra\tESSingleStimMicroelectrode'; % This is the folder where the folder 'data' with segmented data is kept. We assume that badTrials are available also
+session='single';% It can be 'single','dual','dual60'
 
 protocolLists = getAllProtocolLists(subjectName);
 
 % Choose two protocolLists to compare
-group1 = [1 3 5];
-group2 = [2 4 6];
+group1 = 1;
+group2 = 2;
 protocolListsToUse{1} = protocolLists(group1);
 protocolListsToUse{2} = protocolLists(group2);
 
+
 removeStimProtocolFlag = 1;
-displayPowerData(protocolListsToUse,removeStimProtocolFlag);
+displayPowerData2(protocolListsToUse,removeStimProtocolFlag,session);
--- powerProjectCodes/runSavePowerData.m
+++ powerProjectCodes/runSavePowerData.m
@@ -1,12 +1,14 @@
-subjectName = 'dona';
-folderData = 'N:\Projects\Niloy_tES-StimulationProject'; % This is the folder where the folder 'data' with segmented data is kept. We assume that badTrials are available also
+subjectName = 'jojo';
+folderData = 'D:\tESSingleStimMicroelectrode'; % This is the folder where the folder 'data' with segmented data is kept. We assume that badTrials are available also
+session='single';
 
 % Fixed variables
 gridType = 'Microelectrode';
 gridLayout = 2;
 sideChoice = [];
-badTrialNameStr = 'V4';
+badTrialNameStr = 'V4'; % In case of dona its 'V1'
 useCommonBadTrialsFlag = 1;
+commonUnitFlag=1; % In case of dona its 0
 
 % Free variables
 f = 5; o = 5; c = 4; removeERPFlag = 0;
@@ -15,7 +17,6 @@ protocolLists = getAllProtocolLists(subjectName);
 protocolsToSave = 1:8;
 
 for p = 1:length(protocolsToSave)
-    
     % Choose a protocolList
     protocolList = protocolLists{protocolsToSave(p)}; % Choose one
     [expDatesAll,protocolNamesAll] = getProtocolListDetails(protocolList);
@@ -26,6 +27,7 @@ for p = 1:length(protocolsToSave)
         expDate = uniqueExpDates{i}; % Choose one
         protocolNames = protocolNamesAll(strcmp(expDate,expDatesAll));
         goodLFPElectrodes = getGoodLFPElectrodes(subjectName,expDate,folderData,gridType);
-        savePowerData(subjectName,expDate,protocolNames,folderData,gridType,goodLFPElectrodes,protocolList,f,o,c,removeERPFlag);
+        goodSpikeElecs = getGoodSpikeInfo(subjectName,expDate,folderData,protocolNames,badTrialNameStr,session,gridType,commonUnitFlag);
+        savePowerData(subjectName,expDate,protocolNames,folderData,gridType,goodLFPElectrodes,goodSpikeElecs,protocolList,f,o,c,removeERPFlag,badTrialNameStr,useCommonBadTrialsFlag);
     end
 end
